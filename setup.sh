6039ab315dadbbe2bae8f8e269c3f33e23a39b45
Kentaro Kuribayashi <kentarok@gmail.com>
2017-05-02 22:49:00 +0900

    support ~/.config

--- setup.sh
+++ setup.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-for p in `ls -d .[^.]*`; do
-  if [ $p != ".git" ] && ! `echo "$p" | grep -sqE ".swp$"`; then
-    ln -sf "`pwd`/$p" "$HOME/$p" && echo "link: $HOME/$p to `pwd`/$p"
+for p in `ls -A`; do
+  if [ $p != "setup.sh" ] && [ $p != ".git" ] && ! `echo "$p" | grep -sqE ".swp$"`; then
+
+    # .configは既存のものもあるので置き換えない
+    if [ $p == ".config" ]; then
+      for c in `ls -A $p/`; do
+        src="`pwd`/$p/$c"
+        dst="$HOME/$p/$c"
+        rm -f "$dst"
+        ln -sf "$src" "$dst" && echo "link: $dst to $src"
+      done
+
+    # その他のものはそのままリンクする
+    else
+      src="`pwd`/$p"
+      dst="$HOME/$p"
+      rm -f "$dst"
+      ln -sf "$src" "$dst" && echo "link: $dst to $src"
+    fi
   fi
 done
